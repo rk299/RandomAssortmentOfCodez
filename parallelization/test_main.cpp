4723bbcc47f35d18cb2f54c2938be7188362109f
Rahul Kumar <rahul.kumar.iitd@gmail.com>
2010-10-10 17:02:18 -0400

    new example

--- parallelization/test_main.cpp
+++ parallelization/test_main.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 class Sleep : public libutil::UnaryFunctionConst<int,int>
 {
@@ -20,23 +21,62 @@ class Sleep : public libutil::UnaryFunctionConst<int,int>
     }
 };
 
+class Prime : public libutil::UnaryFunctionConst<int,std::vector<int> >
+{
+    public:
+    bool isPrime( const int num ) const
+    {
+        for ( int i = 2; i < sqrt(num); i++ )
+        {
+            if ( num % i == 0 )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    std::vector<int> operator()(const int& limit) const
+    {
+        std::vector<int> primes;
+        assert( limit >= 2 );
+        for ( size_t i = 2; i <= limit; i++ )
+        {
+            if ( isPrime( i ) )
+            {
+                primes.push_back( i );
+            }
+        }
+        std::ostringstream os;
+        os << "\nPrimes of " << limit
+           << " are ";
+        for ( size_t i = 0; i < primes.size() - 1; i++ )
+        {
+            os << primes[i] << ", ";
+        }
+        os << primes[ primes.size() - 1 ];
+        std::cout << os.str() << std::endl;
+        return primes;
+    }
+};
+
 int main(int argc, const char** argv)
 {
-    Sleep s;
+    Prime p;
     assert( argc == 2 );
     unsigned int numComputations = atoi( argv[1] );
     std::vector<int> inputs;
-    std::vector<int> outputs;
+    std::vector<std::vector<int> > outputs;
     outputs.resize( numComputations );
-    for( size_t i = 0; i < numComputations; i++ )
+    for( size_t i = 2; i < numComputations+2; i++ )
     {
         inputs.push_back( i );
     }
-    libutil::ParallelizableComputation<int,int> computation( s,
-                                                             inputs,
-                                                             outputs );
+    libutil::ParallelizableComputation<int,
+            std::vector<int> > computation( p,
+                                            inputs,
+                                            outputs );
     DBG1;
-    libutil::Threads<int,int> threads( computation );
+    libutil::Threads<int,std::vector<int> > threads( computation );
     DBG1;
     threads.activate();
     DBG1;
